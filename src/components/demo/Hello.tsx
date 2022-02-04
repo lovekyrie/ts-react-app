b3bcd436c2208bc5a8c8e3e7b89c337edb33a1f1
duanyupeng <fengdurant@163.com>
2022-02-04 21:44:28 +0800

    modify npm script

--- src/components/demo/Hello.tsx
+++ src/components/demo/Hello.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import {Button} from "antd"
+import { Button } from "antd";
 
 interface Greeting {
-  name:string;
-  firstName:string;
-  lastName:string;
+  name: string;
+  firstName: string;
+  lastName: string;
 }
 
-const Hello=(props:Greeting)=><Button> Hello {props.name} </Button>
+const Hello = (props: Greeting) => <Button> Hello {props.name} </Button>;
 //函数组件会多一个children属性，然后在给组件默认赋值的时候有自动提示，但是需要给未提供的属性增加可选类型
 // const Hello:React.FC<Greeting> = ({name,firstName,lastName,children})=><Button> Hello {name} </Button>
 
-Hello.defaultProps={
-  firstName:'',
-  lastName:''
-}
-export default Hello
+Hello.defaultProps = {
+  firstName: "",
+  lastName: "",
+};
+export default Hello;
